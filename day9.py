ba3a79a4b8dd5cd179cae674c836b0e4c430042c
Robyn <robyn.rintjema@gmail.com>
2024-12-10 16:43:14 -0500

    day9 part 2

--- day9.py
+++ day9.py
@@ -34,6 +34,60 @@ def generateDiscImage(data):
         index+=1
     return disc
     
+def generateDiscImage2(data):
+    index = 0
+    file_id = -1
+    disc = []
+
+    while index < len(data):
+        if index%2== 0:
+            file_id+=1
+            id = file_id
+        else:
+            id = 'Space'
+
+        disc.append({'id':id,'count':int(data[index])})
+        index+=1
+    return disc
+
+def generateDiscImage3(data):
+    index = 0
+    disc = []
+    for d in data:
+        if d["id"]== 'Space':
+            insert = '.'
+        else:
+            insert = d["id"]
+            
+        for i in range(d["count"]):
+            disc.append(insert)
+        index+=1
+    return disc
+    
+def formatDisc(disc):
+    file_ids = [x["id"] for x in disc if x["id"] != "Space"]
+
+    for i in file_ids[::-1]:
+        disc_len=len(disc)-1
+        for d_index,d in enumerate(disc[::-1]):
+            if d["id"] == i:
+                disc_index = disc_len - d_index
+                break
+        for d_index in range(disc_index):
+            d = disc[d_index]
+            if d["id"] == 'Space' and d['count'] >= disc[disc_index]['count']:
+                disc[d_index]['count']=d['count']-disc[disc_index]['count']
+                temp = {
+                    'id':i,
+                    'count':disc[disc_index]['count']
+                }
+                disc[disc_index]["id"] = 'Space'
+                disc.insert(d_index,temp)
+                break
+            else:
+                continue
+    return disc
+
 def part1(data):
     result = 0
 
@@ -61,16 +115,24 @@ def part1(data):
 
 def part2(data):
     result = 0
+    disc = generateDiscImage2(data)
+    disc = formatDisc(disc)
+    disc = generateDiscImage3(disc)
+
+    for i in range(len(disc)):
+        if disc[i] == '.':
+            continue
+        result = result + (i*disc[i])
     return result
 
 def main():
     filename = getFilename()
     data = parseFile(filename)
 
-    part1_result =part1(data)
-    print(part1_result)
+    #part1_result =part1(data)
+    #print(part1_result)
 
-    #part2_result = part2(data)
-    #print(part2_result)
+    part2_result = part2(data)
+    print(part2_result)
 
 main()
