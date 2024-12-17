a9313917ce4ccabbe8cb2a8fb4dc19f33725e399
Robyn <robyn.rintjema@gmail.com>
2024-12-17 15:34:39 -0500

    day15 part 1

---
+++ day15.py
@@ -0,0 +1,101 @@
+'''
+
+
+'''
+import sys
+
+dirs = {
+    '^':(-1,0),
+    'v':(1,0),
+    '<':(0,-1),
+    '>':(0,1)
+}
+
+def getFilename():
+    argc = len(sys.argv)
+    filename = 'input.txt'
+    if argc > 1:
+        filename = 'test_input.txt'
+    return filename
+
+def parseFile(filename):
+    f = open(filename, "r")
+
+    data = f.read()
+
+    data = data.split('\n\n')
+    grid = data[0].split('\n')
+    grid = [list(x) for x in grid]
+    moves = list(data[1].replace('\n',''))
+    
+    return grid,moves
+
+def findGuard(data):
+    max_rows = len(data)
+    max_cols = len(data[0])
+    x = 0
+    for y in range(max_rows):
+        row = data[y]
+        if '@' in row:
+            x = row.index('@')
+            break
+    return (y,x)
+
+def moveBoxes(val,row,col,grid,dir):
+    if grid[row][col]=='#':
+        return grid,False
+    elif grid[row][col]=='O':
+        grid,moved = moveBoxes('O',row+dir[0],col+dir[1],grid,dir)
+        if moved:
+            grid[row][col]=val
+        return grid,moved
+    else:
+        grid[row][col]=val
+        return grid,True
+
+
+def part1(grid,moves):
+    result = 0
+    guard_pos = findGuard(grid)
+
+    for m in moves:
+        m = dirs[m]
+        new_pos = (guard_pos[0]+m[0],guard_pos[1]+m[1])
+        if grid[new_pos[0]][new_pos[1]] == '.':
+            grid[guard_pos[0]][guard_pos[1]] = '.'
+            grid[new_pos[0]][new_pos[1]] = '@'
+            guard_pos = new_pos
+        elif grid[new_pos[0]][new_pos[1]] == '#':
+            continue
+        else:
+            grid,moved = moveBoxes('@',new_pos[0],new_pos[1],grid,m)
+            if moved:
+                grid[guard_pos[0]][guard_pos[1]] = '.'
+                guard_pos = new_pos
+    
+    for y,row in enumerate(grid):
+        for x,col in enumerate(row):
+            if col == 'O':
+                gps = (100*y)+x
+                result += gps
+
+
+        
+    
+    return result
+
+def part2(data):
+    result = 0
+    return result
+
+def main():
+    filename = getFilename()
+    data,moves = parseFile(filename)
+
+    part1_result =part1(data,moves)
+    print(part1_result)
+
+    #part2_result = part2(data)
+    #print(part2_result)
+
+main()
